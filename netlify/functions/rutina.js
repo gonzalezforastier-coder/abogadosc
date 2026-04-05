9e75d21cf3c5accece1f02cb34cb5954109e9501
gonzalezforastier-coder <gonzalezforastier@gmail.com>
2026-04-05 09:22:17 -0300

    Update rutina.js

--- netlify/functions/rutina.js
+++ netlify/functions/rutina.js
@@ -26,7 +26,7 @@ exports.handler = async function(event) {
       },
       body: JSON.stringify({
         model: 'claude-haiku-4-5-20251001',
-        max_tokens: 1500,
+        max_tokens: 2000,
         messages: body.messages,
         system: body.system || ''
       })
